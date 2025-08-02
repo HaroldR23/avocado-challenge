199916e38514cc81298feffb4c018d798d26b04a
HaroldR23 <ahsro99@gmail.com>
2025-08-02 11:16:09 -0300

    feat: create the database models and add the needed dependencies

---
+++ backend/infrastructure/src/adapters/db/sql_alchemy/models/tasks.py
@@ -0,0 +1,33 @@
+from models.base import Base
+
+from typing import TYPE_CHECKING, List
+from enum import Enum
+
+from sqlalchemy import Boolean, DateTime, ForeignKey, Integer, String, Enum as SqlEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class Priority(Enum):
+    LOW = "baja"
+    MEDIUM = "media"
+    HIGH = "alta"
+
+if TYPE_CHECKING:
+	from .users import User
+	from .comments import Comment
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    completed: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    due_date: Mapped[str] = mapped_column(DateTime, nullable=True)
+    priority: Mapped[Priority] = mapped_column(SqlEnum(Priority), nullable=False, default=Priority.LOW)
+
+    created_by_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    assigned_to_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=True)
+
+    created_by: Mapped["User"] = relationship("User", foreign_keys=[created_by_id])
+    assigned_to: Mapped["User"] = relationship("User", foreign_keys=[assigned_to_id])
+    comments: Mapped[List["Comment"]] = relationship()
---
+++ backend/infrastructure/src/adapters/db/sql_alchemy/models/users.py
@@ -0,0 +1,20 @@
+from models.base import Base
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from .roles import Role
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey("roles.id"), nullable=False)
+
+    role: Mapped["Role"] = relationship()
